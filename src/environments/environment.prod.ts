dc8c40428e132c818cb49484a50366a8c7a21d41
Nazeeh Salah <nazeehsalah28@gmail.com>
2023-01-11 01:49:54 +0200

    fix env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  apiKey: 'BmcusDQMjtqODJ2o55QjmKQqlHFPGZUKml46FH5Y',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  apiKey: 'BmcusDQMjtqODJ2o55QjmKQqlHFPGZUKml46FH5Y',
 };
 
 /*
